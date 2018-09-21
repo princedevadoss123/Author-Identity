af31ea00e35a8634a85466b0800d34f979795564
Devadoss <prince.devadoss@sap.com>
2018-09-21 10:49:08 +0530

    Https Server implemented

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var Server = require('./server-module/server');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var routing = require('./routes/router.js');
@@ -11,7 +12,7 @@ var app = express();
 
 app.use(bodyParser.json());
 app.use(bearerToken());
-app.listen(port);
 mongoose.connect(db.url);
 console.log(process.env.CARD_BEARER_TOKEN);
 routing.route(app);
+new Server(3504, app).createServer();
